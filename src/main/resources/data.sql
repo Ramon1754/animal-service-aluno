2baa9e8ba0e702c1afd15238cf4abc2247d44d77
Ramon1754 <114538472+Ramon1754@users.noreply.github.com>
2024-08-16 23:08:54 -0300

    Initial commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,27 @@
+INSERT
+INTO
+  animal
+  (nome_provisorio, idade_estimada, raca, tipo_de_animal, data_entrada, condicoes_chegada, nome_recebedor, porte)
+VALUES
+  ('Cachorrinho da Ebac', 5, 'SRD', 'Cachorro', current_date, 'Bem pulguento', 'Cleber', 'Pequeno');
+
+INSERT
+INTO
+  animal
+  (nome_provisorio, idade_estimada, raca,tipo_de_animal, data_entrada, condicoes_chegada, nome_recebedor, porte)
+VALUES
+  ('Outro cachorrinho da Ebac', 10, 'Rottweiler','Cachorro', current_date -10, 'Bem velinho', 'Renato', 'Grande');
+
+INSERT
+INTO
+  animal
+  (nome_provisorio, idade_estimada, raca, tipo_de_animal, data_entrada, condicoes_chegada, nome_recebedor, porte)
+VALUES
+  ('Totó', 2, 'Pinscher', 'Cachorro', current_date, '50% tremedeira e 50% ódio', 'Giovanna', 'Pequeno', current_date);
+
+INSERT
+INTO
+  animal
+  (nome_provisorio, tipo_de_animal, idade_estimada, raca, data_entrada, condicoes_chegada, nome_recebedor, porte)
+VALUES
+  ('Peludinho', 'Gato', 2, 'Persa', '05/02/2024', 'Meio Sujo mas passa bem', 'Rodrigo', 'Médio');
